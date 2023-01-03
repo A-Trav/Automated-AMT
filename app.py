9a9e7177135779da9de86c21076f349c6476cdef
A-Trav <ashleytravaini@hotmail.com>
2023-01-03 15:44:42 +1000

    Converted the application to a deployment server

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+"""
+The app module: Flask API application.
+
+The root Flask server application.
+"""
+
 from flask import Flask
 
 def create_app(config):
+    from SMTP.smtp import mail
+    from Auth.auth import jwt, bcrypt
     from Database.database import initalize_db, ma
     from Handler.auth import auth
     from Handler.create import create
@@ -10,10 +18,17 @@ def create_app(config):
     from Handler.export import export
     from ScheduledTask.scheduler import scheduler_init_app
     from Utils.startup_tasks import import_snomed_csv
+    from flask import request
+    import logging
 
     # Initialise application
     app = Flask(__name__)
     app.config.from_object(config)
+
+    app.config['DEBUG'] = check_debug_arg()
+
+    if not (app.config['DEBUG'] or app.config['TESTING']):
+        prod_logging()
 
     # Routes
     app.register_blueprint(auth)
@@ -26,15 +41,32 @@ def create_app(config):
     # initialize app components
     ma.init_app(app)
     initalize_db(app)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
 
     if app.config['TESTING'] != True:
         scheduler_init_app(app)
+        mail.init_app(app)
         import_snomed_csv(app)
 
+    @app.before_request
+    def log_request():
+        app.logger.info(f'API call made to: {request.url}')
 
     return app
 
 if __name__ == '__main__':
+    from waitress import serve
     from config import AppConfig
+    from Utils.app_args import check_debug_arg, check_admin_arg
+    from bootstrap_admin import bootstrap_admin
+    from app_logging import prod_logging
+
     app = create_app(AppConfig)
-    app.run(port = 8080, debug = True, use_reloader=False)
+    bootstrap_admin(app, check_admin_arg())
+    
+    if app.config['DEBUG']:
+        app.run(port = 8080, debug = True, use_reloader=False)
+    else:
+        app.logger.info('Starting server')
+        serve(app, host='127.0.0.1', port = 8080, threads = 1)
--- config.py
+++ config.py
@@ -1,8 +1,22 @@
+"""
+The config module: Application configuration.
+
+Provides different configuration classes to run the Flask application with.
+"""
+
 import os
+
 class AppConfig:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'amt.db3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TESTING  = False
+    JWT_SECRET_KEY = os.urandom(24)
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = 465
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_USE_SSL = True
+    MAIL_DEFAULT_SENDER = MAIL_USERNAME
+    TESTING = False
 
 class TestConfig(AppConfig):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
